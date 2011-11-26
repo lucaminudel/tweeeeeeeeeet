b2c85a717f1625404a9613de2c642d99d0bf8fcd
lucaminudel <mymailgateway-github@yahoo.it>
2011-11-26 04:19:58 +0100

    implemented check of tweet's length, when shortening the tweet the case is preserved and whe calculating the tweet's length the urs are counter as shortened

--- tweeeeeeeeeet.js
+++ tweeeeeeeeeet.js
@@ -866,50 +866,106 @@ parser = (function(){
       
 
       var shortener = function() {
-      
+     
+				var tweet = '';
+		
+				function findMatchIgnoringCase(source, searchString, subString) {
+					var lowerSource = source.toLowerCase(source);
+					var lowerSearchString = searchString.toLowerCase(searchString);
+					var indexBegin = lowerSource.indexOf(lowerSearchString);
+					var indexEnd = indexBegin + searchString.length;
+					var match = source.substring(indexBegin, indexEnd);
+					
+					var previousChar = ' ';
+					if (indexBegin > 0) {
+						previousChar = source[indexBegin -1];
+					}
+					var successorChar = ' ';
+					if (indexEnd < source.length) {
+						successorChar = source[indexEnd];
+					}
+					
+					if (subString === false && (!previousChar.match(/\s/) || !successorChar.match(/\s/))) {
+						indexBegin = -1;
+					}
+					
+					return {
+						index: indexBegin,
+						value: match
+					};
+				}
+				
+				function copyCase(from, to) {
+					if (to === from.toUpperCase()) {
+						return to.toUpperCase();
+					}
 
-   
-      
+					if (from === from.toLowerCase()) {
+						return to.toLowerCase();
+					}
+					
+					if (from === from.toUpperCase()) {
+						return to.toUpperCase();
+					}
+					
+					if (from === capitalize(from)) {
+						return capitalize(to);
+					}
+					
+					return to;
+				}
+				
+				function capitalize(source) {
+					return source.charAt(0).toUpperCase() + source.slice(1);
+				}
 
-               var tweet = '';
-      
+				function tweetWithShortenedUrlsLength(tweet) {
+					var urlRegExp = /(ht|f)tp(s?)\:\/\/[0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*(:(0-9)*)*(\/?)([a-zA-Z0-9\-\.\?\,\'\/\\\+&amp;%\$#_]*)?/;
+					var length = tweet.length;
+					var shortUrlsLength = 0;
+					
+					var match;
+					while (match = tweet.match(urlRegExp)) {
+					
+						tweet = tweet.replace(urlRegExp, 'x');
+						length = tweet.length;
+						shortUrlsLength += 19;
+					}
+					
+					return (length + shortUrlsLength);
+				}
 
-   
-      
+				function tweetFits(tweet) {					
+					return tweetWithShortenedUrlsLength(tweet) <= 140;
+				}
+				
+				return {
+				
+					shortenTweet: function(str) { tweet = str; },
 
-               return {
-      
+					replaceUntilFit: function(replaceMaps) {
 
-                 shortenTweet: function(str) { tweet = str; },
+					for(var i = 0; i < replaceMaps.length; ++i) {   
+						var subString = replaceMaps[i][0];
+						var from = replaceMaps[i][1];
+						var to = replaceMaps[i][2];
+						var match = findMatchIgnoringCase(tweet, from, subString);
+						while (match.index >= 0)  {						
+      																			
+							tweet = tweet.replace(match.value, copyCase(match.value, to));						
+							match = findMatchIgnoringCase(tweet, from);							
+						
+							if (tweetFits(tweet)) {
+								return tweet;
+							}
+						}      
+					}	
       
-
-                 replaceUntilFit: function(replaceMaps) {
+					return tweet;
       
-
-                   for(var i = 0; i < replaceMaps.length; ++i) {
+                }
       
-
-                      while(tweet.indexOf(replaceMaps[i][1])>= 0) {
-      
-
-                        tweet = tweet.replace(replaceMaps[i][1], replaceMaps[i][2]);
-      
-
-                      }
-      
-
-                   }
-      
-
-                   return tweet;
-      
-
-                 }
-      
-
-               }
-      
-
+            }      
       }();
       
 
--- tweeeeeeeeeet.pegjs
+++ tweeeeeeeeeet.pegjs
@@ -1,21 +1,106 @@
 {
+      var shortener = function() {
+     
+				var tweet = '';
+		
+				function findMatchIgnoringCase(source, searchString, subString) {
+					var lowerSource = source.toLowerCase(source);
+					var lowerSearchString = searchString.toLowerCase(searchString);
+					var indexBegin = lowerSource.indexOf(lowerSearchString);
+					var indexEnd = indexBegin + searchString.length;
+					var match = source.substring(indexBegin, indexEnd);
+					
+					var previousChar = ' ';
+					if (indexBegin > 0) {
+						previousChar = source[indexBegin -1];
+					}
+					var successorChar = ' ';
+					if (indexEnd < source.length) {
+						successorChar = source[indexEnd];
+					}
+					
+					if (subString === false && (!previousChar.match(/\s/) || !successorChar.match(/\s/))) {
+						indexBegin = -1;
+					}
+					
+					return {
+						index: indexBegin,
+						value: match
+					};
+				}
+				
+				function copyCase(from, to) {
+					if (to === from.toUpperCase()) {
+						return to.toUpperCase();
+					}
 
-   var shortener = function() {
+					if (from === from.toLowerCase()) {
+						return to.toLowerCase();
+					}
+					
+					if (from === from.toUpperCase()) {
+						return to.toUpperCase();
+					}
+					
+					if (from === capitalize(from)) {
+						return capitalize(to);
+					}
+					
+					return to;
+				}
+				
+				function capitalize(source) {
+					return source.charAt(0).toUpperCase() + source.slice(1);
+				}
 
-            var tweet = '';
+				function tweetWithShortenedUrlsLength(tweet) {
+					var urlRegExp = /(ht|f)tp(s?)\:\/\/[0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*(:(0-9)*)*(\/?)([a-zA-Z0-9\-\.\?\,\'\/\\\+&amp;%\$#_]*)?/;
+					var length = tweet.length;
+					var shortUrlsLength = 0;
+					
+					var match;
+					while (match = tweet.match(urlRegExp)) {
+					
+						tweet = tweet.replace(urlRegExp, 'x');
+						length = tweet.length;
+						shortUrlsLength += 19;
+					}
+					
+					return (length + shortUrlsLength);
+				}
 
-            return {
-              shortenTweet: function(str) { tweet = str; },
-              replaceUntilFit: function(replaceMaps) {
-                for(var i = 0; i < replaceMaps.length; ++i) {
-                   while(tweet.indexOf(replaceMaps[i][1])>= 0) {
-                     tweet = tweet.replace(replaceMaps[i][1], replaceMaps[i][2]);
-                   }
+				function tweetFits(tweet) {					
+					return tweetWithShortenedUrlsLength(tweet) <= 140;
+				}
+				
+				return {
+				
+					shortenTweet: function(str) { tweet = str; },
+
+					replaceUntilFit: function(replaceMaps) {
+
+					for(var i = 0; i < replaceMaps.length; ++i) {   
+						var subString = replaceMaps[i][0];
+						var from = replaceMaps[i][1];
+						var to = replaceMaps[i][2];
+						var match = findMatchIgnoringCase(tweet, from, subString);
+						while (match.index >= 0)  {						
+      																			
+							tweet = tweet.replace(match.value, copyCase(match.value, to));						
+							match = findMatchIgnoringCase(tweet, from);							
+						
+							if (tweetFits(tweet)) {
+								return tweet;
+							}
+						}      
+					}	
+      
+					return tweet;
+      
                 }
-                return tweet;
-              }
-            }
-   }();
+      
+            }      
+      }();
 }
 
  
