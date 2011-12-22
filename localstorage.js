4ca8beaec569b90b78fdc147e3fbca4695a69d38
lucaminudel <mymailgateway-github@yahoo.it>
2011-12-22 16:51:51 +0100

    Added Save and Load persistence functions in the coockies

--- localstorage.js
+++ localstorage.js
@@ -8,29 +8,26 @@
 
 var tweeeeeeeeeet = (tweeeeeeeeeet === null || typeof tweeeeeeeeeet === 'undefined') ? {} : tweeeeeeeeeet;
 
-if (!window.localStorage) {
-  tweeeeeeeeeet.localStorage = {
-    getItem: function (sKey) {
-      if (!sKey || !this.hasOwnProperty(sKey)) { return null; }
-      return unescape(document.cookie.replace(new RegExp("(?:^|.*;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*((?:[^;](?!;))*[^;]?).*"), "$1"));
-    },
-    key: function (nKeyId) { return unescape(document.cookie.replace(/\s*\=(?:.(?!;))*$/, "").split(/\s*\=(?:[^;](?!;))*[^;]?;\s*/)[nKeyId]); },
-    setItem: function (sKey, sValue) {
-      if(!sKey) { return; }
-      document.cookie = escape(sKey) + "=" + escape(sValue) + "; path=/";
-      this.length = document.cookie.match(/\=/g).length;
-    },
-    length: 0,
-    removeItem: function (sKey) {
-      if (!sKey || !this.hasOwnProperty(sKey)) { return; }
-      var sExpDate = new Date();
-      sExpDate.setDate(sExpDate.getDate() - 1);
-      document.cookie = escape(sKey) + "=; expires=" + sExpDate.toGMTString() + "; path=/";
-      this.length--;
-    },
-    hasOwnProperty: function (sKey) { return (new RegExp("(?:^|;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie); }
-  };
-  tweeeeeeeeeet.localStorage.length = (document.cookie.match(/\=/g) || tweeeeeeeeeet.localStorage).length;
-} else {
-  tweeeeeeeeeet.localStorage = window.localStorage;
+tweeeeeeeeeet.localStorage = {
+	getItem: function (sKey) {
+	  if (!sKey || !this.hasOwnProperty(sKey)) { return null; }
+	  return unescape(document.cookie.replace(new RegExp("(?:^|.*;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*((?:[^;](?!;))*[^;]?).*"), "$1"));
+	},
+	key: function (nKeyId) { return unescape(document.cookie.replace(/\s*\=(?:.(?!;))*$/, "").split(/\s*\=(?:[^;](?!;))*[^;]?;\s*/)[nKeyId]); },
+	setItem: function (sKey, sValue) {
+	  if(!sKey) { return; }
+	  document.cookie = escape(sKey) + "=" + escape(sValue) + "; path=/";
+	  this.length = document.cookie.match(/\=/g).length;
+	},
+	length: 0,
+	removeItem: function (sKey) {
+	  if (!sKey || !this.hasOwnProperty(sKey)) { return; }
+	  var sExpDate = new Date();
+	  sExpDate.setDate(sExpDate.getDate() - 1);
+	  document.cookie = escape(sKey) + "=; expires=" + sExpDate.toGMTString() + "; path=/";
+	  this.length--;
+	},
+	hasOwnProperty: function (sKey) { return (new RegExp("(?:^|;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie); }
+	};
+	tweeeeeeeeeet.localStorage.length = (document.cookie.match(/\=/g) || tweeeeeeeeeet.localStorage).length;
 }
